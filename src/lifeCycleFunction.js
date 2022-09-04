d7a79e4cba7b14c34ca6fc4dc808bc24f930eb04
lukitoandriansyah <lukitoandriansyah45@gmail.com>
2022-09-05 01:50:49 +0700

    test tambah ke keranjang function

--- src/lifeCycleFunction.js
+++ src/lifeCycleFunction.js
@@ -1,4 +1,6 @@
 //Misal kita memiliki data API seperti berikut ini:
+import {useEffect, useState} from "react";
+
 const dataApi = [
     {
         namaBarang: "gitar",
@@ -28,11 +30,108 @@ const dataApi = [
 ]
 
 function LifeCycleFunction(){
+    //Initialisai State di Function Component
+    const [jumlahItemGitar, setJumlahItemGitar] = useState(0);
+    const [jumlahItemBass, setJumlahItemBass] = useState(0);
+    const [jumlahItemPiano, setJumlahItemPiano] = useState(0);
+    const [jumlahItemDrum, setJumlahItemDrum] = useState(0);
+    const [jumlahItemSexophone, setJumlahItemSexophone] = useState(0);
+    const [totalHarga, setTotalHarga] = useState(0);
+    const [dataAlat, setDataAlat] = useState([]);
+    const [isiKeranjang, setIsiKeranjang] = useState([]);
+
+    function tambahKeKeranjang(alatMusikYangDipilih) {
+        const isiKeranjangSekarang = [...isiKeranjang];
+        isiKeranjangSekarang.push(alatMusikYangDipilih);
+        isiKeranjangSekarang.sort(function (a, b) {
+            let x = a.namaBarang.toLowerCase();
+            let y = b.namaBarang.toLowerCase();
+            if (x < y) {
+                return -1;
+            }
+            if (x > y) {
+                return 1;
+            }
+            return 0
+        })
+        //this.setState({isiKeranjang: isiKeranjangSekarang});
+        setIsiKeranjang(isiKeranjangSekarang);
+        console.log(isiKeranjang);
+    }
+
+    function hapusDariDaftarKeranjang(alatMusikYangDipilih) {
+        const isiKeranjangSekarang = [...isiKeranjang];
+        isiKeranjangSekarang.splice(isiKeranjangSekarang.indexOf(alatMusikYangDipilih), 1);
+        //this.setState({isiKeranjang: isiKeranjangSekarang});
+        setIsiKeranjang(isiKeranjangSekarang);
+        console.log(isiKeranjangSekarang);
+    }
+
+    useEffect(()=>{
+        setDataAlat(dataApi)
+    }, [])
+
+    useEffect(()=>{
+        let hargaTotal = 0;
+        let itemGitar = 0;
+        let itemBass = 0;
+        let itemPiano = 0;
+        let itemDrum = 0;
+        let itemSexophone = 0;
+        for (let daftarIsiKeranjang of isiKeranjang) {
+            hargaTotal = hargaTotal + daftarIsiKeranjang.hargaBarang
+            if (daftarIsiKeranjang.namaBarang === "gitar") {
+                itemGitar = itemGitar + 1
+            }
+            if (daftarIsiKeranjang.namaBarang === "bass") {
+                itemBass = itemBass + 1
+            }
+            if (daftarIsiKeranjang.namaBarang === "keyboard") {
+                itemPiano = itemPiano + 1
+            }
+            if (daftarIsiKeranjang.namaBarang === "drum") {
+                itemDrum = itemDrum + 1
+            }
+            if (daftarIsiKeranjang.namaBarang === "sexophone") {
+                itemSexophone = itemSexophone + 1
+            }
+        }
+        setTotalHarga(hargaTotal);
+        setJumlahItemGitar(itemGitar);
+        setJumlahItemBass(itemBass);
+        setJumlahItemDrum(itemDrum);
+        setJumlahItemPiano(itemPiano);
+        setJumlahItemSexophone(itemSexophone);
+
+    }, [isiKeranjang])
+
     return(
         <>
-            <p>Total Harga</p>
+            <p>Total Harga: {totalHarga}</p>
             <h4>Daftar Produk</h4>
+            <ul>
+                {dataAlat.map((namaAlatMusik)=>
+                <li>
+                    {namaAlatMusik.namaBarang} | <span> </span>
+                    {namaAlatMusik.hargaBarang} | <span> </span>
+                    <button onClick={()=>tambahKeKeranjang(namaAlatMusik)}>
+                        + Keranjang
+                    </button>
+                </li>
+                )}
+            </ul>
             <h4>Daftar Keranjang</h4>
+            <ul>
+                {isiKeranjang.map((daftarIsiKeranjang)=>
+                    <li>
+                        {daftarIsiKeranjang.namaBarang} | <span> </span>
+                        {daftarIsiKeranjang.hargaBarang} | <span> </span>
+                        <button /*onClick={()=>tambahKeKeranjang(namaAlatMusik)}*/>
+                            + Keranjang
+                        </button>
+                    </li>
+                )}
+            </ul>
         </>
     )
 }
